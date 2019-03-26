491c6e80c4272d56d18ac82dc87805fba8457514
avf <avf@avf.avf>
2019-03-26 21:59:02 +0300

    callculator

--- Linux/Laboratory_3/1.sh
+++ Linux/Laboratory_3/1.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 if [ $# = 3 ]
 then
-    case $2 in
+	case $2 in
 	+) 	res=$(echo "scale=25;$1+$3" | bc)
 		;;
 	-)	res=$(echo "scale=25;$1-$3" | bc)
 		;;
 	x|X)res=$(echo "scale=25;$1*$3" | bc)
 		;;
-	/)	res=$(echo "scale=25;$1/$3" | bc)
+	/)	if [ $3 -eq 0 ]; then
+	            res="divine 0"
+		else
+		res=$(echo "scale=25;$1/$3" | bc)
+		fi
 		;;
 	*)	res="use right operands: + - / x"
 		;;
