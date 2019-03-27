fc0900f774ef405f56852d32d5663f7d04668fef
avf <avf@avf.avf>
2019-03-27 21:08:05 +0300

    complite lab

--- Linux/Laboratory_3/1.sh
+++ Linux/Laboratory_3/1.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
+
+
+re='^[0-9]+$'
+nul=$(echo "$3 == 0.0" | bc)
+#echo $nul
+if [ $4 ]; then
+echo "much argument"
+exit
+fi
+
 if [ $# = 3 ]
 then
+if ( echo "$3" | grep -E -q "?[0-9]+$" ) ; then
+if ( echo "$1" | grep -E -q "?[0-9]+$" ) ; then
+    a=2
+    else
+echo "no num"
+    exit
+fi
+
 	case $2 in
 	+) 	res=$(echo "scale=25;$1+$3" | bc)
 		;;
@@ -11,13 +29,20 @@ then
 	/)	if [ $3 -eq 0 ]; then
 	            res="divine 0"
 		else
+		if [ $nul -eq 1 ]; then
+		    res="divine 0"
+		else
 		res=$(echo "scale=25;$1/$3" | bc)
+		fi
 		fi
 		;;
 	*)	res="use right operands: + - / x"
 		;;
     esac
 	echo $res
+else
+    echo "no num"
+fi
 else
         echo "wrong arguments"
 fi
