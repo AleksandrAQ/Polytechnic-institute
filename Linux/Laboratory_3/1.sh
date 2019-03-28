069364cd796ce2736e75f26bece99047e0979750
Aleksandr <avferonok@corp.ifmo.ru>
2019-03-28 11:25:22 +0300

    1.sh

--- Linux/Laboratory_3/1.sh
+++ Linux/Laboratory_3/1.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
-
-
-re='^[0-9]+$'
+#калькулятор с проверкой вводимых значений
+reg='^[0-9]+$'
 nul=$(echo "$3 == 0.0" | bc)
-#echo $nul
+
 if [ $4 ]; then
 echo "much argument"
 exit
 fi
 
-if [ $# = 3 ]
-then
-if ( echo "$3" | grep -E -q "?[0-9]+$" ) ; then
-if ( echo "$1" | grep -E -q "?[0-9]+$" ) ; then
-    a=2
-    else
-echo "no num"
+if ! ( echo "$3" | grep -E -q "?[0-9]+$" ) ; then
+    echo "no num"
     exit
 fi
 
+if ! ( echo "$1" | grep -E -q "?[0-9]+$" ) ; then
+    echo "no num"
+    exit
+fi
+
+if [ $# = 3 ]
+then
 	case $2 in
 	+) 	res=$(echo "scale=25;$1+$3" | bc)
 		;;
@@ -29,11 +30,11 @@ fi
 	/)	if [ $3 -eq 0 ]; then
 	            res="divine 0"
 		else
-		if [ $nul -eq 1 ]; then
-		    res="divine 0"
-		else
-		res=$(echo "scale=25;$1/$3" | bc)
-		fi
+		    if [ $nul -eq 1 ]; then
+			res="divine 0"
+		    else
+			res=$(echo "scale=25;$1/$3" | bc)
+		    fi
 		fi
 		;;
 	*)	res="use right operands: + - / x"
@@ -41,8 +42,6 @@ fi
     esac
 	echo $res
 else
-    echo "no num"
-fi
-else
         echo "wrong arguments"
 fi
+
