48790bd66a672f1a89b584fceaf6e666ea850c9c
Aleksandr <avferonok@corp.ifmo.ru>
2019-05-06 11:46:55 +0300

    complite ex5

--- Fundamentals_of_algorithmization/Exercises_5/src/main.f90
+++ Fundamentals_of_algorithmization/Exercises_5/src/main.f90
@@ -3,10 +3,8 @@ program exercise_5
    
    implicit none
    character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt"
-   integer                 :: In = 0, Out = 0, N = 0, M = 0, i = 0
-   integer                 :: S = 0
+   integer                 :: In = 0, Out = 0, N = 0
    integer, allocatable    :: Z(:)
-   logical, allocatable    :: Pos(:)
 
    open (file=input_file, newunit=In)
       read (In, *) N
@@ -14,16 +12,9 @@ program exercise_5
       read (In, *) Z
    close (In)
 
-    do i = 1, N
-	 S = mod(Z(i),2) ! написать сечение и каждому элементу присвоить ноль
-         If (S == 0) then
-            Z(i)= 0
-         end if
-    end do
+   Z(2:N:2)=0
 
    open (file=output_file, encoding=E_, newunit=Out)
-      write (Out, "(i0)") N
       write (Out, "("//N//"(i0, 1x))") Z
    close (Out)
-
 end program exercise_5
